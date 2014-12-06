f1844ab6feb3cd4acd0bac7815ed32ccbae3ccf1
Anna Akullian <annaakullian@Annas-MacBook-Air-2.local>
2014-12-05 16:57:42 -0800

    fixing for heroku

--- swap.py
+++ swap.py
@@ -557,8 +557,8 @@ def vote_yes(item_id):
 
 
 if __name__ == '__main__':
-	PORT = int(os.environ.get(“PORT”, 5000))
-	app.run(debug=True, host=“0.0.0.0”, port=PORT)
+	PORT = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host="0.0.0.0", port=PORT)
 
 
 
